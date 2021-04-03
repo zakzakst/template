36d9704145b72a8519a01bc6126509584a1b2fc0
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-04-03 15:14:29 +0900

    EJSビルド作成

--- ejs/task/ejs-build.js
+++ ejs/task/ejs-build.js
@@ -4,7 +4,12 @@
  * npm modules
  */
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const ejs = require('gulp-ejs');
+const extender = require('gulp-html-extend');
+const htmlhint = require('gulp-htmlhint');
+const htmlbeautify = require('gulp-html-beautify');
+const htmlmin = require('gulp-htmlmin');
 const rename = require('gulp-rename');
 
 /**
@@ -15,6 +20,7 @@ const files = [
   '!./src/ejs/html/**/_*.ejs',
 ];
 const dist = './dist';
+const options = require('../html-options');
 
 /**
  * function
@@ -22,7 +28,13 @@ const dist = './dist';
 function EJS_BUILD() {
   return gulp
     .src(files)
+    .pipe(plumber())
+    .pipe(extender({ annotations: false }))
     .pipe(ejs({}, {}, { ext: '.html' }))
+    .pipe(htmlhint(options.hint))
+    .pipe(htmlhint.reporter())
+    .pipe(htmlbeautify(options.beautify))
+    // .pipe(htmlmin(options.minify))
     .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest(dist));
 }
