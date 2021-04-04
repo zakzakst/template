0f9cc5c85773064d562cb57755a1f8fd15be3f19
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-04-04 23:37:04 +0900

    SASSビルド

--- ejs/gulpfile.js
+++ ejs/gulpfile.js
@@ -10,9 +10,11 @@ const gulp = require('gulp');
  */
 const IMAGE_MIN = require('./task/image-min');
 const EJS_BUILD = require('./task/ejs-build');
+const SASS_BUILD = require('./task/sass-build');
 
 /**
  * gulp tasks
  */
 gulp.task('imageMin', gulp.series(IMAGE_MIN));
 gulp.task('ejsBuild', gulp.series(EJS_BUILD));
+gulp.task('sassBuild', gulp.series(SASS_BUILD));
